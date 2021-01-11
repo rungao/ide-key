b46ff1476b587ed741eea0d49865fdda5be4e660
高旭 <gaox@2345.com>
2021-01-11 10:34:48 +0800

    jetbrains series ide key get

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rungao/ide-key
+
+go 1.14
+
+require github.com/atotto/clipboard v0.1.2
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"archive/zip"
+	"github.com/atotto/clipboard"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+const (
+	licensedUrl = "http://idea.medeming.com/jets/images/jihuoma.zip"
+	zipFile     = "idea.medeming.com.zip"
+)
+
+func main() {
+
+	// 下载文件内容并保存
+	response, err := http.Get(licensedUrl)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	os.Remove(zipFile)
+	ioutil.WriteFile(zipFile, data, 0644)
+
+	// 获取压缩包内容
+	r, err := zip.OpenReader(zipFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var license string
+	for k, f := range r.File {
+		if k > 0 {
+			continue
+		}
+		f, err := f.Open()
+
+		if err != nil {
+			panic(err)
+		}
+		fd, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic(err)
+		}
+		err = f.Close()
+		if err != nil {
+			panic(err)
+		}
+		license = string(fd)
+	}
+
+	// 拷贝到粘贴板
+	clipboard.WriteAll(string(license))
+
+	r.Close()
+	if err != nil {
+		panic(err)
+	}
+	dir, err := os.Getwd()
+	err = os.Remove(dir + "/" + zipFile)
+	if err != nil {
+		panic(err)
+	}
+
+	// 设置到ide licensed中
+
+}
